f70d94706a354630d4d55a04b2d55c47c97acb29
Willian Pitz <willian.pitz@calcard.com.br>
2020-12-23 19:10:55 -0300

    Ajustes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Iniciando o projeto no Windows
 
 1. Fazer o clone do projeto.
-2. Executar o comando ```npm i``` nas pastas frontend e backend,para instalar as dependencias do projeto.
-3. Executar o comando ```npm start``` na pasta frontend para subir o nosso Front-end, e executar o mesmo comando na pasta backend, para subir também o nosso Back-End.
+2. Executar o comando ```npm i``` na pasta frontend, para instalar as dependencias do projeto.
+3. Executar o comando ```npm start .env.local``` na pasta frontend para subir o nosso Front-end.
 
 ### Lembre-se: Ao sair da área onde você estava acampado, deixe-a mais limpa do que quando a encontrou 😂
 
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -9,18 +9,18 @@ import './styles.css';
 import Swapi from '../../services/Swapi';
 import Header from '../../components/Header';
 
-export default function Detail(props){
+export default function Detail(props) {
 
-  const [itemDetail, setItemdetailInfo] = useState([]);
+  const [itemDetail, setItemdetailInfo] = useState( null );
 
-  useEffect(()=> {
+  useEffect(() => {
     const loadDetailList = async () => {
       let detailInfo = await Swapi.getDetailInfo(props.location.pathInfo);
       setItemdetailInfo(detailInfo[0].items)
     }
 
     loadDetailList();
-  }, []);
+  }, [props.location.pathInfo]);
 
   return (
     <>
@@ -28,32 +28,42 @@ export default function Detail(props){
 
       <div className="detailContainer">
         <div className="infoBox">
-        {itemDetail.length <= 0 &&
-          <div className="loading">
-            <div className="loadingText">
-              <strong>Loading...</strong>
-            </div>
+          {!itemDetail &&
+            <div className="loading">
+              <div className="loadingText">
+                <strong>Loading...</strong>
+              </div>
               <img src={gif} alt="loading-gif" />
-          </div>
+            </div>
+          }
+
+          {itemDetail &&
+            (
+              Object.keys(itemDetail).slice(0, 5).map((key) => (
+                <div key={key} className="infoItem">
+                  <span>{key}:</span>
+                  <span>{itemDetail[key]}</span>
+                </div>
+              ))
+            )
+          }
+        </div>
+  
+        {itemDetail && 
+          (
+            <div className="detailImage">
+              <img src={`/${props.location.imgSrc}/detail/${itemDetail.name || itemDetail.title}.png`} onError={(e) => e.target.src = `/${props.location.imgSrc}/detail/avatar-icon.png`} alt={itemDetail.name || itemDetail.title} />
+            </div>
+          )
         }
-        {Object.keys(itemDetail).slice(0, 5).map((key) => (
-          <div key={key} className="infoItem">
-            <span>{key}:</span>
-            <span>{itemDetail[key]}</span>
-          </div>
-          ))}
-        </div>
-        <div className="detailImage">
-          <img src={`/${props.location.imgSrc}/detail/${itemDetail.name || itemDetail.title}.png`} onError={(e) => e.target.src=`/${props.location.imgSrc}/detail/avatar-icon.png`} alt={itemDetail.name || itemDetail.title} />
-        </div>
 
       </div>
-        <Link to="/">
+      <Link to="/">
         <button className="backIcon">
-          <KeyboardBackspaceIcon style={{ fontSize: 30}} />
-            <span>Voltar</span>
+          <KeyboardBackspaceIcon style={{ fontSize: 30 }} />
+          <span>Voltar</span>
         </button>
-        </Link>
+      </Link>
     </>
   );
 }
